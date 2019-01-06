00b461816bbfea8032c5c63afb30cbe849e0695f
volkswg <volkswg@hotmail.com>
2019-01-06 16:38:34 +0700

    Java FX
    Java Fx + Password Encrypt + Java Fx Css

---
+++ JAVA/06 - JAVA FX/PasswordEncyption.java
@@ -0,0 +1,51 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
+
+public class PasswordEncyption {
+	private byte[] salt;
+	public PasswordEncyption() throws NoSuchAlgorithmException, NoSuchProviderException {
+		this.salt = getSalt();
+	}
+	
+	public String genPassHash(String passwordIn) {
+		return getSecurePassword(passwordIn, this.salt);
+	}
+	
+	private String getSecurePassword(String passwordToHash, byte[] salt)
+    {
+        String generatedPassword = null;
+        try {
+            // Create MessageDigest instance for MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            //Add password bytes to digest
+            md.update(salt);
+            //Get the hash's bytes
+            byte[] bytes = md.digest(passwordToHash.getBytes());
+            //This bytes[] has bytes in decimal format;
+            //Convert it to hexadecimal format
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++)
+            {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            //Get complete hashed password in hex format
+            generatedPassword = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return generatedPassword;
+    }
+     
+    //Add salt
+	private byte[] getSalt() throws NoSuchAlgorithmException, NoSuchProviderException
+    {
+        //Always use a SecureRandom generator
+        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG", "SUN");
+        byte[] salt = new byte[16];
+        sr.nextBytes(salt);
+        return salt;
+    }
+}
